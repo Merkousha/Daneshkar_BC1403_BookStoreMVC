24cb4ff383064d9fabfd32ff7a2535d81bf310d2
merkousha <Merkousha.net@gmail.com>
2025-02-11 07:50:55 +0330

    Add BookDTO

--- Daneshkar_BC1403_BookStoreMVC/Pages/Index.cshtml.cs
+++ Daneshkar_BC1403_BookStoreMVC/Pages/Index.cshtml.cs
@@ -10,17 +10,25 @@ namespace Daneshkar_BC1403_BookStoreMVC.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly RefhubContext _context;
 
+
+
         public IndexModel(ILogger<IndexModel> logger, RefhubContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-        public List<Book> books { get; set; }
+        public class BookDTO { 
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string? Thumb { get; set; }
+            public string? Year { get; set; }
+        }
+        public List<BookDTO> books { get; set; }
         public string something { get; set; }
         public async Task OnGetAsync()
         {
-            books = await _context.Books.Where(x => x.Id < 50).ToListAsync();
+            books = await _context.Books.Where(x => x.Id < 50).Select(b => new BookDTO { Name= b.Name, Id= b.Id, Thumb=  b.Thumb, Year= b.Year }).ToListAsync();
         }
     }
 }
